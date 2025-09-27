a22ce94c2856c43f7b935b68a9229f8eed05f3f9
Anjana Modugu <anjanamodugu@gmail.com>
2025-09-27 22:13:20 +0530

    added vip pic in squidgame scene

--- VR Carnival/Assets/Scripts/SceneLoader.cs
+++ VR Carnival/Assets/Scripts/SceneLoader.cs
@@ -5,8 +5,6 @@ public class SceneLoader : MonoBehaviour
 {
     public string sceneName; // name of the scene to load
 
-    public GameObject WebSocketManager; 
-
     public void LoadTargetScene()
     {
         SceneManager.LoadScene(sceneName);
